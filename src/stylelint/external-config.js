627625048fc2006fbd2a1a487df77038dbb7467f
Omri Lavi <omrila@monday.com>
2022-04-06 17:23:57 +0300

    added tests for the rule

--- src/stylelint/external-config.js
+++ src/stylelint/external-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  customSyntax: "postcss-scss",
   plugins: ["./rules/use-defined-css-var-when-available/index.js"],
   rules: {
     "monday-ui-style/use-defined-css-var-when-available": true,
