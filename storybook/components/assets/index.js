14f4c64f9eae093c6b5d94eb7f597aff26d840ad
Sergey Roytman <sergeyro@monday.com>
2023-07-27 17:56:25 +0300

    accessibility.stories.mdx

---
+++ storybook/components/assets/index.js
@@ -0,0 +1,6 @@
+import clarity from "./clarity.png";
+import delight from "./delight.png";
+import speed from "./speed.png";
+import path from "./path.png";
+
+export { clarity, delight, speed, path };
