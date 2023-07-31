16d2f58c13728eb7de6aef911aecc261630620ee
Sergey Roytman <sergeyro@monday.com>
2023-07-31 18:03:27 +0300

    Welcome page story

--- storybook/components/assets/index.js
+++
@@ -1,6 +0,0 @@
-import clarity from "./clarity.png";
-import delight from "./delight.png";
-import speed from "./speed.png";
-import path from "./path.png";
-
-export { clarity, delight, speed, path };
